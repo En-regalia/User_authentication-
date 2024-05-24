0a1235f33a11f53e4fa0884e8f7ce6730c5fdb55
En-regalia <mike.ranner1@gmail.com>
2024-05-24 10:23:42 +0100

    Updated file structure

--- Frontend/app.py
+++ Frontend/app.py
@@ -12,9 +12,3 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 
-mybd = _mysql_connector.connect(
-    host = 'localhost'
-    user = 'yourusername'
-    password = 'your_password'
-)
-
